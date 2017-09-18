73c251e1bc6c1f243a795cc6b4131e2a7c0b103b
jimynu <jimynu@Kims-Luftbuch.lan>
2017-09-18 16:18:18 +0200

    v1

---
+++ week1/day1/6_countRepetitions.js
@@ -0,0 +1,35 @@
+// Count repetitions
+//
+// You will be given an array of string:
+//
+// ['kerouac', 'fante', 'fante', 'buk', 'hemingway', 'hornby', 'kerouac', 'buk', 'fante']
+// Return an object where the keys are the string and the value for each key is how many repetitions they have in the provided array
+//
+//
+// In the previous example the solution would be:
+//
+// {
+//   kerouac: 2,
+//   fante: 3,
+//   buk: 2,
+//   hemingway: 1,
+//   hornby: 1
+// }
+
+function countRepetitions(authorsArr) {
+  var authors = {};
+
+  for(var key in authorsArr) {
+    var author = [authorsArr[key]];
+    if(authors[author]) {
+      // increase author count
+      authors[author]++;
+    } else {
+      // create author and set count to 1
+      authors[author] = 1;
+    }
+  }
+  console.log(authors);
+}
+
+countRepetitions(['kerouac', 'fante', 'fante', 'buk', 'hemingway', 'hornby', 'kerouac', 'buk', 'fante']);
