1acdfe8d680984dc82fab4aad495d035c3c02bd5
jimynu <jimynu@Kims-Luftbuch.lan>
2017-09-19 09:10:50 +0200

    day1 revision

--- week1/day1/11_fibonacci.js
+++ week1/day1/11_fibonacci.js
@@ -14,28 +14,29 @@ function fib(n) {
 }
 
 
-// function fibs(n) {
-//   var result = [];
-//   for (var i = 0; i < n; i++) {
-//     result.push(fib(i));
-//   }
-//   return result;
-// }
-//
-//
-// function fibs(n) {
-//   if (n === 1) {
-//     return [0];
-//   }
-//   if (n === 2) {
-//     return [0, 1];
-//   }
-//
-//   var prevFibs = fibs(n - 1);
-//   var lastIndex = prevFibs.length - 1;
-//
-//   return prevFibs.concat([prevFibs[lastIndex] + prevFibs[lastIndex - 1]]);
-// }
+function fibs(n) {
+  var result = [];
+  for (var i = 0; i < n; i++) {
+    result.push(fib(i));
+  }
+  return result;
+}
+
+// provided
+function fibs(n) {
+  if (n === 1) {
+    return [0];
+  }
+  if (n === 2) {
+    return [0, 1];
+  }
+
+  var prevFibs = fibs(n - 1);
+  var lastIndex = prevFibs.length - 1;
+
+  // concat(): adds element to array
+  return prevFibs.concat([prevFibs[lastIndex] + prevFibs[lastIndex - 1]]);
+}
 
 console.log(fibs(3)); // => [0, 1, 1]
 console.log(fibs(7)); // => [0, 1, 1, 2, 3, 5, 8]
