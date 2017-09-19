bd65650a050a44648148f0e042d9b6855ee2c50c
jimynu <jimynu@Kims-Luftbuch.lan>
2017-09-19 14:29:47 +0200

    trivial 0.2: play more than 1 questions

--- week1/day2/trivial.js
+++ week1/day2/trivial.js
@@ -1,10 +1,8 @@
 // constructor for class Trivial
 function Trivial() {
   this.questions = [];
+  this.currentQstnIndex = 0;
 }
-
-// class
-var trivial = new Trivial();
 
 // method: add question
 Trivial.prototype.addQuestion = function(qstn) {
@@ -12,19 +10,37 @@ Trivial.prototype.addQuestion = function(qstn) {
   console.log(Trivial.questions);
 };
 
-// method: ask question
-Trivial.prototype.askQuestion = function(qstn) {
+// method: ask one question
+Trivial.prototype.askQuestion = function() {
   // display the challenge
-  var userAnswer = window.prompt(qstn.title + '\n' + qstn.choices.join('\n'));
+  var question = this.questions[this.currentQstnIndex];
+  var userAnswer = window.prompt(question.display());
 
   // check if answer is right
-  if (userAnswer.toUpperCase() === qstn.correctAnswer) {
+  if (userAnswer.toUpperCase() === question.correctAnswer) {
     window.alert('Congrats!');
   } else {
     window.alert('Wrong answer, my dear.');
   }
 };
 
+// method: increase current question index
+Trivial.prototype.next = function() {
+  this.currentQstnIndex++;
+};
+
+// method: ask all questions
+Trivial.prototype.play = function() {
+  this.askQuestion();
+  // set current question to next one
+  this.next();
+  // if there are more questions, play again
+  if ( this.currentQstnIndex < this.questions.length ) {
+    this.play();
+  } else {
+    console.log('game over');
+  }
+};
 
 // constructor for class Question
 function Question(title, choices, correctAnswer) {
@@ -33,10 +49,23 @@ function Question(title, choices, correctAnswer) {
   this.correctAnswer = correctAnswer;
 }
 
+// method: format the question for output
+Question.prototype.display = function() {
+  return this.title + '\n' + this.choices.join('\n');
+};
+
+
+
+// start game: create instance
+var trivial = new Trivial();
+
 // initialise questions
 var question = new Question('Capital of France', ['A - Paris', 'B - Rome'], 'A');
 trivial.addQuestion(question);
+var question2 = new Question('Capital of Sweden', ['A - Paris', 'B - Stockholm'], 'B');
+trivial.addQuestion(question2);
 console.log(trivial.questions);
 
 // start the game
-trivial.askQuestion(question);
+//trivial.askQuestion(question);
+trivial.play();
